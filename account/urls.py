f9902437d79a2f05491424134bc8336205534f84
Bhawani-08 <bhawanipatidar08@gmail.com>
2024-04-05 17:56:19 +0530

    Initial commit

---
+++ account/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from account.views import SendPasswordResetEmailView, UserRegistrationView,UserLoginView,UserProfileView,UserChangepasswordView
+urlpatterns = [
+    path("register/",UserRegistrationView.as_view(),name = 'register'),
+    path("login/",UserLoginView.as_view(),name = 'login'),
+    path("profile/",UserProfileView.as_view(),name = 'profile'),
+    path("changepassword/",UserChangepasswordView.as_view(),name = 'changepassword'),
+    path("send-reset-password-email/",SendPasswordResetEmailView.as_view(),name = 'send-reset-password-email'),
+
+
+
+    
+]
